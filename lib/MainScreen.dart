f900f457991a5fc5e3f61f898158d3c38c56bbb1
Yash Gupta <yashg160@gmail.com>
2020-01-23 21:39:51 +0530

    Picture Retrieval Done

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'dart:typed_data';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class Picture {
+  String title;
+  String base64;
+  String id;
+  Uint8List bytes;
+
+  Picture({this.id, this.title, this.base64, this.bytes});
+
+  getTitle() {
+    return this.title;
+  }
+
+  getContent() {
+    return this.bytes;
+  }
+
+  getId() {
+    return this.id;
+  }
+}
 
 class MainScreen extends StatefulWidget {
   MainScreenState createState() => MainScreenState();
@@ -12,7 +34,6 @@ class MainScreenState extends State<MainScreen> {
   bool _error = false;
   String _errMessage = '';
   List _pictures = [];
-  BuildContext _context;
 
   _getPictures() async {
     var response = await http.get('http://10.102.113.91:8000/picture?num=10');
@@ -28,13 +49,36 @@ class MainScreenState extends State<MainScreen> {
       }
 
       // There was no error. Then extract the image data from the response.
-      var pictures = data['pictures'];
+      List pictures = data['pictures'];
       return pictures;
     } else {
       // Some error occurred. Throw Exception to handle in the calling function
       _errMessage = 'ERR_SERVER';
       throw Exception('ERR_SERVER');
     }
+  }
+
+  _getImagesFromBase64(pictures) async {
+    // Pictures is an array containing pictures encoded as base64 images, along with their titles and id.
+    // The class Picture handles the conversion to bit-array and other stuff. Create Picture objects and return an array.
+
+    List finalPictures = [];
+
+    for (var pic in pictures) {
+      String content = pic['content'];
+      String base64String = content.split(',')[1];
+
+      Uint8List bytes = convert.base64Decode(base64String);
+      Picture picture = new Picture(
+          id: pic['_id'],
+          title: pic['title'],
+          base64: base64String,
+          bytes: bytes);
+
+      _pictures.add(picture);
+    }
+
+    return finalPictures;
   }
 
   _handleGetPictures() {
@@ -45,13 +89,19 @@ class MainScreenState extends State<MainScreen> {
     });
 
     _getPictures().then((pictures) {
+      if (pictures.length == 0) {
+        _errMessage = 'ERR_NO_PICTURES';
+        throw Exception('ERR_NO_PICTURES');
+      }
+      print('Picture length: ${pictures.length}');
+      _getImagesFromBase64(pictures);
+    }).then((finalPictures) {
       setState(() {
-        _pictures = pictures;
         _loading = false;
         _error = false;
       });
-
-      print(_pictures.length);
+      print('Pictures set in state');
+      print('Length of pictures in state: ${_pictures.length}');
     }).catchError((error) {
       setState(() {
         _loading = false;
@@ -76,17 +126,6 @@ class MainScreenState extends State<MainScreen> {
             label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
       ),
     );
-  }
-
-  Widget _mainContent(BuildContext context) {
-    return (Scaffold(
-      appBar: AppBar(
-        title: Text('ReactStock'),
-      ),
-      body: Center(
-        child: Text('Data was retrived. No error'),
-      ),
-    ));
   }
 
   @override
@@ -122,21 +161,24 @@ class MainScreenState extends State<MainScreen> {
       ));
     } else {
       return (Scaffold(
-          body: Builder(
-        builder: (context) => ModalProgressHUD(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('ReactStock'),
-            ),
-            body: Center(
-              child: Text('Data was retrived. No error'),
+        body: Builder(
+          builder: (context) => ModalProgressHUD(
+            inAsyncCall: _loading,
+            opacity: 0.9,
+            progressIndicator: CircularProgressIndicator(),
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text('ReactStock'),
+              ),
+              body: Container(
+                child: Center(
+                  child: Text('No error'),
+                ),
+              ),
             ),
           ),
-          inAsyncCall: _loading,
-          opacity: 0.9,
-          progressIndicator: CircularProgressIndicator(),
         ),
-      )));
+      ));
     }
   }
 }
