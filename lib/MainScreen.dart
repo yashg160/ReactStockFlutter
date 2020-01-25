2760400bd040f23781d43c8283b17e94d9e1ef72
Yash Gupta <yashg160@gmail.com>
2020-01-25 15:34:26 +0530

    Created Arguments Class

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -26,6 +26,21 @@ class Picture {
   }
 }
 
+class Arguments {
+  String title;
+  Uint8List bytes;
+
+  Arguments({this.title, this.bytes});
+
+  getTitle() {
+    return this.title;
+  }
+
+  getContent() {
+    return this.bytes;
+  }
+}
+
 class CustomPopupMenu {
   CustomPopupMenu({this.title, this.action});
 
