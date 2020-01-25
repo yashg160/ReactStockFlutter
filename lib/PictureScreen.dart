2d8b83666ca72f6539730775298aa85361a795d3
Yash Gupta <yashg160@gmail.com>
2020-01-25 15:42:07 +0530

    Completed Picture Screen

--- lib/PictureScreen.dart
+++ lib/PictureScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:react_stock_flutter/Arguments.dart';
 
@@ -5,16 +7,16 @@ class PictureScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Arguments args = ModalRoute.of(context).settings.arguments;
-    print(args.getTitle());
-    print(args.getContent());
+    final Uint8List picture = args.getContent();
+
     return Scaffold(
       backgroundColor: Theme.of(context).accentColor,
-      /* body: Center(
+      body: Center(
         child: Image.memory(
           picture,
           fit: BoxFit.contain,
         ),
-      ), */
+      ),
     );
   }
 }
